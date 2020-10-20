171cdafc734bdd593e7521116e78157874dfe0e3
Mickaël Canévet <mickael.canevet@gmail.com>
2020-10-20 18:11:25 +0200

    Fix vault

--- camptocamp-devops-stack/distributions/k3s/_/docker/scripts/destroy-before-script.sh
+++ camptocamp-devops-stack/distributions/k3s/_/docker/scripts/destroy-before-script.sh
@@ -1,21 +1,21 @@
 #!/bin/sh -xe
 
-# cd vault || exit
-# terraform init -upgrade
-# terraform workspace select "$CLUSTER_NAME" || terraform workspace new "$CLUSTER_NAME"
-# terraform init -upgrade
-# terraform destroy --auto-approve
-# if [ "$CLUSTER_NAME" != "default" ]; then
-# 	terraform workspace select default
-# 	terraform workspace delete "$CLUSTER_NAME"
-# fi
-# cd -
+cd camptocamp-devops-stack/vault || exit
+terraform init -upgrade
+terraform workspace select "$CLUSTER_NAME" || terraform workspace new "$CLUSTER_NAME"
+terraform init -upgrade
+terraform destroy --auto-approve
+if [ "$CLUSTER_NAME" != "default" ]; then
+	terraform workspace select default
+	terraform workspace delete "$CLUSTER_NAME"
+fi
+cd -
 
 wget https://storage.googleapis.com/kubernetes-release/release/v1.19.0/bin/linux/amd64/kubectl -O ~/kubectl
 chmod +x ~/kubectl
 
 # Remove all Applications to prevent ArgoCD to reconcile
-# ~/kubectl -n argocd delete application --all
+~/kubectl -n argocd delete application --all
 
 # Remove all Operators to ensure that it does not redeploy stuffs
 ~/kubectl -n cluster-operators delete deployments --all
